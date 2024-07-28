0ad49aed507c5864ea867e3a07e25b26032f3009
atrox3d <atrox3d@gmail.com>
2024-07-28 20:17:35 +0200

    tests/test_sqlite_db.py

---
+++ tests/test_sqlite_db.py
@@ -0,0 +1,98 @@
+from db.sqlitedb import SqliteDb
+from pathlib import Path
+import pytest
+
+@pytest.fixture
+def sqlite_db_path():
+    return 'test.db'
+
+@pytest.fixture
+def db(sqlite_db_path):
+    return SqliteDB(sqlite_db_path)
+
+@pytest.fixture
+def date():
+    return '2024/08/01'
+
+@pytest.fixture
+def title():
+    return 'geforce now'
+
+@pytest.fixture
+def records():
+    return [['2024/08/01', 'geforce now'] for _ in range(10)]
+
+
+
+def test_can_instantiate_sqlitefb(sqlite_db_path):
+    db = SqliteDb(sqlite_db_path)
+
+def test_create_empty_db(db, sqlite_db_path):
+    db.create()
+    assert Path(sqlite_db_path).exists()
+
+def test_init_and_load_empty_db(db):
+    assert db.data == []
+
+def test_load_empty_db(db):
+    db.load()
+    assert db.data == []
+
+def test_save_empty_db(db):
+    db.save()
+    db.load()
+    assert db.data == []
+
+def test_add_title(db, date, title):
+    db.add(date, title)
+    assert db.data == [{'date': date, 'title': title}]
+
+def test_save_and_reload_title(db, date, title):
+    db.add(date, title)
+    db.save()
+    db.load()
+    assert db.data == [{'date': date, 'title': title}]
+
+def test_populate_db(db, records):
+    db.populate(records)
+    expected = [dict(date=date, title=title) for date, title in records]
+    assert db.data == expected
+
+def test_populate_save_and_load(db, records):
+    db.populate(records)
+    db.save()
+    db.load()
+    expected = [dict(date=date, title=title) for date, title in records]
+    assert db.data == expected
+
+def test_populate_rows(db, records):
+    db.populate(records)
+    expected = [dict(date=date, title=title) for date, title in records]
+    assert list(db.rows()) == expected
+
+def test_populate_save_load_rows(db, records):
+    db.populate(records)
+    db.save()
+    db.load()
+    expected = [dict(date=date, title=title) for date, title in records]
+    assert list(db.rows()) == expected
+
+def test_titles(db):
+    db.add('20000101', 'Doom')
+    db.add('20000101', 'Quake')
+    print(db.titles())
+    assert db.titles() == ['Doom', 'Quake']
+
+def test_find_title(db):
+    db.add('20000101', 'Doom')
+    db.add('20000101', 'Quake')
+    assert db.find_title('doom') == 'Doom'
+
+def test_title_like(db):
+    db.add('20000101', 'id Doom')
+    db.add('20000101', 'id Quake')
+    assert db.title_like('id') == ['id Doom', 'id Quake']
+
+
+
+
