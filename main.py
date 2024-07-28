7d3321c310996fa88f468dbd165c44bd19592dea
atrox3d <atrox3d@gmail.com>
2024-07-28 13:46:45 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,44 +1,9 @@
-from pathlib import Path
-from bs4 import BeautifulSoup
-import datetime
-from typing import Generator
-
-# import requests
-
-def get_html(filepath:str|Path) -> str:
-    try:
-        with open(filepath, encoding="utf-8") as fp:
-            contents = fp.read()
-    except FileNotFoundError as fnfe:
-        print(f"ERROR| {fnfe}\nexiting...")
-        exit(1)
-    except UnicodeDecodeError as ude:
-        print(f"ERROR| {ude}\nexiting...")
-        exit(2)
-    return contents
-
-def get_soup(filepath:str|Path) -> BeautifulSoup:
-    html = get_html(filepath)
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
-
-def format_date(date:str) -> str:
-    m, d, y = date.split('/')
-    return f'{y}/{m:>02}/{d:>02}'
-
-def get_date_and_title(soup:BeautifulSoup) -> Generator[list[datetime.datetime | str], None, None]:
-    table = soup.select('table')[0]
-    rows = table.select('[class=am-0]')
-    for row in rows:
-        tds = row.select('td')[:2]
-        txt = [td.text for td in tds]
-        if len(txt):
-            dt, title = txt
-            yield dt, title
+import scraping
+import formatting
 
 if __name__ == '__main__':
-    soup = get_soup('transactions.html')
+    soup = scraping.get_soup('transactions.html')
 
-    rows = get_date_and_title(soup)
+    rows = scraping.get_date_and_title(soup)
     for dt, title in rows:
-        print(format_date(dt), title)
+        print(formatting.format_date(dt), title)
