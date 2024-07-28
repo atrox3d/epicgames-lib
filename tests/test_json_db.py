e4c5eb828e739644fdadf3e94ba9b9baba0ca5d6
atrox3d <atrox3d@gmail.com>
2024-07-28 16:26:18 +0200

    comment inits

--- tests/test_json_db.py
+++ tests/test_json_db.py
@@ -1,41 +1,44 @@
 from db.json import JsonDb
 from pathlib import Path
+import pytest
 
+@pytest.fixture
+def json_db_path():
+    return 'test.json'
 
-JSON_DB_PATH = 'test.json'
+@pytest.fixture
+def db(json_db_path):
+    return JsonDb(json_db_path)
 
-def test_can_instantiate_jsondb():
-    db = JsonDb(JSON_DB_PATH)
+def test_can_instantiate_jsondb(json_db_path):
+    db = JsonDb(json_db_path)
 
-def test_create_empty_db():
-    db = JsonDb(JSON_DB_PATH)
+def test_create_empty_db(db, json_db_path):
     db.create()
-    assert Path(JSON_DB_PATH).exists()
+    assert Path(json_db_path).exists()
 
-def test_init_and_load_empty_db():
-    db = JsonDb(JSON_DB_PATH, load=True)
+def test_init_and_load_empty_db(db):
     assert db.data == []
 
-def test_load_empty_db():
-    db = JsonDb(JSON_DB_PATH)
+def test_load_empty_db(db):
     db.load()
     assert db.data == []
 
-def test_save_empty_db():
-    db = JsonDb(JSON_DB_PATH)
+def test_save_empty_db(db):
+    # db = JsonDb(json_db_path)
     db.save()
     db.load()
     assert db.data == []
 
-def test_add_title():
-    db = JsonDb(JSON_DB_PATH)
+def test_add_title(db):
+    # db = JsonDb(json_db_path)
     date = '2024/08/01'
     title = 'geforce now'
     db.add(date, title)
     assert db.data == [{'date': date, 'title': title}]
 
-def test_save_and_reload_title():
-    db = JsonDb(JSON_DB_PATH)
+def test_save_and_reload_title(db):
+    # db = JsonDb(json_db_path)
     date = '2024/08/01'
     title = 'geforce now'
     db.add(date, title)
@@ -43,15 +46,15 @@ def test_save_and_reload_title():
     db.load()
     assert db.data == [{'date': date, 'title': title}]
 
-def test_populate_db():
-    db = JsonDb(JSON_DB_PATH)
+def test_populate_db(db):
+    # db = JsonDb(json_db_path)
     records = [['2024/08/01', 'geforce now'] for _ in range(10)]
     db.populate(records)
     expected = [dict(date=date, title=title) for date, title in records]
     assert db.data == expected
 
-def test_populate_save_and_load():
-    db = JsonDb(JSON_DB_PATH)
+def test_populate_save_and_load(db):
+    # db = JsonDb(json_db_path)
     records = [['2024/08/01', 'geforce now'] for _ in range(10)]
     db.populate(records)
     db.save()
@@ -59,15 +62,15 @@ def test_populate_save_and_load():
     expected = [dict(date=date, title=title) for date, title in records]
     assert db.data == expected
 
-def test_populate_rows():
-    db = JsonDb(JSON_DB_PATH)
+def test_populate_rows(db):
+    # db = JsonDb(json_db_path)
     records = [['2024/08/01', 'geforce now'] for _ in range(10)]
     db.populate(records)
     expected = [dict(date=date, title=title) for date, title in records]
     assert list(db.rows()) == expected
 
-def test_populate_save_load_rows():
-    db = JsonDb(JSON_DB_PATH)
+def test_populate_save_load_rows(db):
+    # db = JsonDb(json_db_path)
     records = [['2024/08/01', 'geforce now'] for _ in range(10)]
     db.populate(records)
     db.save()
@@ -75,21 +78,21 @@ def test_populate_save_load_rows():
     expected = [dict(date=date, title=title) for date, title in records]
     assert list(db.rows()) == expected
 
-def test_titles():
-    db = JsonDb(JSON_DB_PATH)
+def test_titles(db):
+    # db = JsonDb(json_db_path)
     db.add('20000101', 'Doom')
     db.add('20000101', 'Quake')
     print(db.titles())
     assert db.titles() == ['Doom', 'Quake']
 
-def test_find_title():
-    db = JsonDb(JSON_DB_PATH)
+def test_find_title(db):
+    # db = JsonDb(json_db_path)
     db.add('20000101', 'Doom')
     db.add('20000101', 'Quake')
     assert db.find_title('doom') == 'Doom'
 
-def test_title_like():
-    db = JsonDb(JSON_DB_PATH)
+def test_title_like(db):
+    # db = JsonDb(json_db_path)
     db.add('20000101', 'id Doom')
     db.add('20000101', 'id Quake')
     assert db.title_like('id') == ['id Doom', 'id Quake']
