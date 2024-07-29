3d5f1eef763c058aa914ad88a34e12bbe7f63ac6
atrox3d <atrox3d@gmail.com>
2024-07-30 01:52:25 +0200

    use jsondb

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import scraping
 import formatting
+from db.db import Db
+from db.jsondb import JsonDb
+
+
 
 if __name__ == '__main__':
     soup = scraping.get_soup('transactions.html')
 
     rows = scraping.get_date_and_title(soup)
-    for dt, title in rows:
-        print(formatting.format_date(dt), title)
+    # for dt, title in rows:
+    #     print(formatting.format_date(dt), title)
+    db = JsonDb('epicgames-library.json')
+    db.create(rows)
+    match = db.title_like('tomb')
+    print(match)
